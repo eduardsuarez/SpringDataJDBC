ce3319f9018c911a20d0c616c2e4e20589d5a245
Eduard Suárez <eduardsb21@outlook.com>
2022-09-07 11:09:52 -0500

    Código final para el ejercicio con Spring Data JDBC

---
+++ src/main/java/com/ejercicio/SpringDataJDBC/RepositorioPersona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ejercicio.SpringDataJDBC;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author eduardsuarez
+ */
+public interface RepositorioPersona extends CrudRepository<Persona, Long>{
+    
+}
